a18b527900e675b7c04c8b9742a08e41579f56ea
Tolja Zubow <zubow@tkn.tu-berlin.de>
2016-10-04 14:36:09 +0200

    working on topo

---
+++ topology/README.md
@@ -0,0 +1,10 @@
+WiSHFUL topology
+================
+
+## Packages
+
+    pip3 install networkx
+    pip3 install numpy
+    pip3 install matplotlib
+    
+## Running
--- topology/topo_discovery.py
+++ topology/topo_discovery.py
@@ -1,5 +1,9 @@
 import logging
 import datetime
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
 import wishful_upis as upis
 from wishful_agent.core import wishful_module
 from wishful_agent.timer import TimerEventSender
@@ -90,3 +94,26 @@ class WiFiTopologyController(wishful_module.ControllerModule):
             node = self.nodes[event.ap_uuid]
 
         self.log.info("RX WiFiGetServingAPReplyEvent: %s, %s, %s, %s" % (event.sta_mac_addr, event.wifi_intf, event.ap_uuid, node.hostname))
+
+        #G = nx.DiGraph()
+
+        #G.add_edges_from([('A', 'B'),('C','D'),('G','D')], weight=1)
+        #G.add_edges_from([('D','A'),('D','E'),('B','D'),('D','E')], weight=2)
+        #G.add_edges_from([('B','C'),('E','F')], weight=3)
+        #G.add_edges_from([('C','F')], weight=4)
+
+
+        #val_map = {'A': 1.0,
+        #                   'D': 0.5714285714285714,
+        #                              'H': 0.0}
+        #
+        #values = [val_map.get(node, 0.45) for node in G.nodes()]
+        #edge_labels=dict([((u,v,),d['weight'])
+        #                 for u,v,d in G.edges(data=True)])
+        #red_edges = [('C','D'),('D','A')]
+        #edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()]
+        #
+        #pos=nx.spring_layout(G)
+        #nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+        #nx.draw(G,pos, node_color = values, node_size=1500,edge_color=edge_colors,edge_cmap=plt.cm.Reds)
+        #pylab.show()
