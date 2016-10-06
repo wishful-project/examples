cc1fc038a31a1f9326f243c1f8b2840fbd8ccfdc
pruckebusch <peter.ruckebusch@intec.ugent.be>
2016-10-06 22:27:33 +0200

    Update flash_nodes.sh

--- contiki/binaries/flash_nodes.sh
+++ contiki/binaries/flash_nodes.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 export PATH=$PATH:/opt/compilers/mspgcc-4.7.3/bin
-echo $PATH
-NODE_ID=$1
-if [ $# = 2 ] && [ -e $2 ]; then
-        BINARY=$2
+if [ $# = 3 ] && [ -e $1 ]; then
+        BINARY=$1
+        NODE_ID=$2
+        SERIAL_DEV=$3
+        echo "INFO: tos-set-symbols --objcopy msp430-objcopy --objdump msp430-objdump --target ihex $BINARY ./test_$NODE_ID.ihex node_id=$NODE_ID"
+        tos-set-symbols --objcopy msp430-objcopy --objdump msp430-objdump --target ihex $BINARY ./test_$NODE_ID.ihex node_id=$NODE_ID
+        echo "INFO: ibcn-f5x-tos-bsl -c $SERIAL_DEV -5 -R --invert-reset --swap-reset-test -r -e -I -p ./test_$NODE_ID.ihex &"
+        ./ibcn-f5x-tos-bsl -c $SERIAL_DEV -5 -R --invert-reset --swap-reset-test -r -e -I -p ./test_$NODE_ID.ihex &      
 else
-        BINARY="./Wishful-application.rm090"
-        echo "INFO: No binary specified, defaulting to $BINARY"
+        echo "Wrong arguments: $BINARY,$NODE_ID,$SERIAL_DEV or PATH $PATH"
 fi
-
-N_NODES=`ls -l /dev/ttyUSB* | wc -l`
-for (( i=0; i<$N_NODES; i++ ))
-do
-        echo "INFO: tos-set-symbols --objcopy msp430-objcopy --objdump msp430-objdump --target ihex $BINARY ./test_$[ i + NODE_ID ].ihex node_id=$[ i + NODE_ID ]"
-        echo "INFO: ibcn-f5x-tos-bsl -c /dev/ttyUSB$i -5 -R --invert-reset --swap-reset-test -r -e -I -p ./test_$[ i + NODE_ID ].ihex&"
-        tos-set-symbols --objcopy msp430-objcopy --objdump msp430-objdump --target ihex $BINARY ./test_$[ i + NODE_ID ].ihex node_id=$[ i + NODE_ID ]
-        /groups/portable-ilabt-iminds-be/wishful/repos/contiki/tools/rm090/ibcn-f5x-tos-bsl -c /dev/ttyUSB$i -5 -R --invert-reset --swap-reset-test -r -e -I -p ./test_$[ i + NODE_ID ].ihex&
-done
