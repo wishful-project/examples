ee2fa70982a0eac351a9d14170f33183f779ecc8
Peter Ruckebusch <peter.ruckebusch@hotmail.com>
2016-10-10 15:55:29 +0200

    making control engine a class attribute

--- contiki/contiki_helpers/global_node_manager.py
+++ contiki/contiki_helpers/global_node_manager.py
@@ -6,12 +6,12 @@ import wishful_controller
 
 from contiki_helpers.local_monitoring_cp import local_monitoring_program
 from contiki_helpers.node_manager import NodeManager
-    
+
 
 class GlobalNodeManager(NodeManager):
-	
-	control_engine = wishful_controller.Controller()
-    
+
+    control_engine = wishful_controller.Controller()
+
     def __init__(self, config_file_path):
         super(GlobalNodeManager, self).__init__(config_file_path, "global")
         self.connected_nodes = {}
@@ -22,13 +22,13 @@ class GlobalNodeManager(NodeManager):
 
         control_engine.load_config(self.config)
         control_engine.start()
-    
+
     #~ def __get_macaddress_by_nodeid_iface(self,node_id, iface):
         #~ for addr in self.mac_address_list:
             #~ if self.mac_address_to_node_id[addr] == node_id and self.mac_address_to_interface[addr] == iface:
                 #~ return addr
         #~ return -1
-    
+
     def __hc_message_handler(hc_connector, mac_address, node_id, iface):
         while True:
             msg = hc_connector.recv(block=False, timeout=1)
@@ -81,8 +81,8 @@ class GlobalNodeManager(NodeManager):
             self.mac_address_to_event_cb[mac_address] = event_callback
             msg = {'interface': [self.mac_address_to_interface[mac_address]], 'command': 'SUBSCRIBE_EVENT', 'upi_type': upi_type, 'event_key_list': event_key_list, 'event_duration': event_duration}
             self.mac_address_to_hc_connector[mac_address].send(msg)
-            
-    
+
+
     def get_measurements_periodic(self, upi_type, measurement_key_list, collect_period, report_period, num_iterations, report_callback, mac_address_list=None):
         if mac_address_list is None:
             mac_address_list = self.mac_address_list
@@ -90,7 +90,7 @@ class GlobalNodeManager(NodeManager):
             self.mac_address_to_report_cb[mac_address] = report_callback
             msg = {'interface': [self.mac_address_to_interface[mac_address]], 'command': 'GET_MEASUREMENTS_PERIODIC', 'upi_type': upi_type, 'measurement_key_list': measurement_key_list, 'collect_period': collect_period, 'report_period': report_period, 'num_iterations': num_iterations}
             self.mac_address_to_hc_connector[mac_address].send(msg)
-    
+
     def __update_mac_address_list(self, node_id):
         radio_platforms = GlobalNodeManager.control_engine.node(node_id).blocking(True).iface("lowpan0").radio.get_radio_platforms()
         for radio_platform in radio_platforms:
@@ -98,7 +98,7 @@ class GlobalNodeManager(NodeManager):
             self.mac_address_list.append(mac_addr)
             self.mac_address_to_node_id[mac_addr] = node_id
             self.mac_address_to_interface[mac_addr] = radio_platform
-    
+
     @GlobalNodeManager.control_engine.new_node_callback()
     def new_node(node):
         self.connected_nodes[node.id] = node
@@ -114,13 +114,13 @@ class GlobalNodeManager(NodeManager):
                 if self.mac_address_to_node_id[mac_address] == node.id:
                     mac_address_exit_list.append(mac_address)
                     del self.mac_address_to_node_id[mac_address]
-                    del self.mac_address_to_interface[mac_address] 
+                    del self.mac_address_to_interface[mac_address]
                     self.mac_address_list.remove(mac_address)
                     for group_name in self.groups:
                         self.group[group_name].remove_node(mac_address)
             del self.connected_nodes[node.id]
         print("NodeExit : NodeID : {} MAC_ADDR : {} Reason : {}".format(node.id,mac_address_exit_list, reason))
-    
+
     def wait_for_agents(ip_address_list, timeout=60):
         for i in range(0, timeout):
             num_matches = 0
@@ -134,9 +134,9 @@ class GlobalNodeManager(NodeManager):
                     log.info("Connected nodes: %s", nodes)
                     return True
             log.info("Still waiting for %d nodes", len(ip_address_list) - num_matches)
-            gevent.sleep(1) 
+            gevent.sleep(1)
         return False
-    
+
     def execute_upi_function(self, upi_type, upi_fname, mac_address_list=None, *args, **kwargs):
         ret = {}
         if mac_address_list is None:
@@ -149,7 +149,7 @@ class GlobalNodeManager(NodeManager):
                 iface = self.self.mac_address_to_interface[mac_address]
                 ret[mac_address] = GlobalNodeManager.control_engine.blocking(True).node(node).iface(iface).exec_cmd(upi_type=upi_type, fname=upi_fname, args=args, kwargs=kwargs)
         return ret
-    
+
     def schedule_upi_function(self, upi_type, upi_fname, exec_time, mac_address_list=None, callback=None, *args, **kwargs):
         ret = {}
         if mac_address_list is None:
@@ -162,7 +162,7 @@ class GlobalNodeManager(NodeManager):
                 iface = self.self.mac_address_to_interface[mac_address]
                 ret[mac_address] = GlobalNodeManager.control_engine.blocking(False).exec_time(exec_time).callback(callback).node(node).iface(iface).exec_cmd(upi_type=upi_type, fname=upi_fname, args=args, kwargs=kwargs)
         return ret
-    
+
     def delay_upi_function(self, upi_type, upi_fname, delay, mac_address_list=None, callback=None, *args, **kwargs):
         ret = {}
         if mac_address_list is None:
