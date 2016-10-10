4bc5fc5810a96c41939dc0b273b29cbb532bf542
Peter Ruckebusch <peter.ruckebusch@hotmail.com>
2016-10-10 18:38:20 +0200

    reverting class attr, now control engine must be created in CP

--- contiki/global_cp.py
+++ contiki/global_cp.py
@@ -34,6 +34,62 @@ __copyright__ = "Copyright (c) 2016, Technische Universität Berlin"
 __version__ = "0.1.0"
 __email__ = "peter.ruckebusch@intec.ugent.be"
 
+control_engine = wishful_controller.Controller()
+global_node_manager = GlobalNodeManager(control_engine)
+
+@control_engine.new_node_callback()
+def new_node(node):
+    global_node_manager.add_node(node)
+    print("New node appeared:")
+    print(node)
+
+@control_engine.node_exit_callback()
+def node_exit(node, reason):
+    global_node_manager.remove_node(node)
+    print("NodeExit : NodeID : {} MAC_ADDR : {} Reason : {}".format(node.id,mac_address_exit_list, reason))
+
+@control_engine.set_default_callback()
+def default_callback(group, node, cmd, data):
+    print("{} DEFAULT CALLBACK : Group: {}, NodeName: {}, Cmd: {}, Returns: {}".format(datetime.datetime.now(), group, node.name, cmd, data))
+
+@control_engine.add_callback(upis.radio.set_rxchannel)
+def set_channel_reponse(group, node, data):
+    print("{} set_channel_reponse : Group:{}, NodeId:{}, msg:{}".format(datetime.datetime.now(), group, node.id, data))
+
+@control_engine.add_callback(upis.radio.get_rxchannel)
+def get_channel_reponse(group, node, data):
+    print("{} get_channel_reponse : Group:{}, NodeId:{}, msg:{}".format(datetime.datetime.now(), group, node.id, data))
+
+def print_response(group, node, data):
+    print("{} Print response : Group:{}, NodeIP:{}, Result:{}".format(datetime.datetime.now(), group, node.ip, data))
+
+def main(args):
+
+    config_file_path = args['--config']
+    config = None
+    with open(config_file_path, 'r') as f:
+        config = yaml.load(f)
+    control_engine.load_config(self.config)
+    control_engine.start()
+    contiki_nodes = []
+
+    #control loop
+    while True:
+        gevent.sleep(10)
+        contiki_nodes = global_node_manager.get_mac_address_list()
+        print("\n")
+        print("Connected nodes", [str(node) for node in contiki_nodes])
+        if contiki_nodes:
+            #execute UPI function blocking
+            global_node_manager.execute_upi_function("radio","set_parameters",contiki_nodes,{'IEEE802154_phyCurrentChannel':12})
+
+            #schedule non-blocking UPI function with specific callback
+            exec_time = datetime.datetime.now() + datetime.timedelta(seconds=3)
+            global_node_manager.schedule_upi_function("radio","set_parameters",exec_time, contiki_nodes, print_response, {'IEEE802154_phyCurrentChannel':12})
+
+            #delayed UPI function call with default callback
+            global_node_manager.delay_upi_function("radio","set_parameters",3, contiki_nodes, {'IEEE802154_phyCurrentChannel':12})
+
 if __name__ == "__main__":
     try:
         from docopt import docopt
@@ -63,62 +119,6 @@ if __name__ == "__main__":
     log = logging.getLogger('contiki_global_control_program')
 
     log.debug(args)
-
-    def main(args):
-
-        config_file_path = args['--config']
-        config = None
-        with open(config_file_path, 'r') as f:
-            config = yaml.load(f)
-        control_engine = wishful_controller.Controller()
-        control_engine.load_config(self.config)
-        control_engine.start()
-        global_node_manager = GlobalNodeManager(control_engine)
-        contiki_nodes = []
-
-        @control_engine.new_node_callback()
-        def new_node(node):
-            global_node_manager.add_node(node)
-            print("New node appeared:")
-            print(node)
-
-        @control_engine.node_exit_callback()
-        def node_exit(node, reason):
-            global_node_manager.remove_node(node)
-            print("NodeExit : NodeID : {} MAC_ADDR : {} Reason : {}".format(node.id,mac_address_exit_list, reason))
-
-        @global_node_manager.control_engine.set_default_callback()
-        def default_callback(group, node, cmd, data):
-            print("{} DEFAULT CALLBACK : Group: {}, NodeName: {}, Cmd: {}, Returns: {}".format(datetime.datetime.now(), group, node.name, cmd, data))
-
-        @global_node_manager.control_engine.add_callback(upis.radio.set_rxchannel)
-        def set_channel_reponse(group, node, data):
-            print("{} set_channel_reponse : Group:{}, NodeId:{}, msg:{}".format(datetime.datetime.now(), group, node.id, data))
-
-        @global_node_manager.control_engine.add_callback(upis.radio.get_rxchannel)
-        def get_channel_reponse(group, node, data):
-            print("{} get_channel_reponse : Group:{}, NodeId:{}, msg:{}".format(datetime.datetime.now(), group, node.id, data))
-
-
-        def print_response(group, node, data):
-            print("{} Print response : Group:{}, NodeIP:{}, Result:{}".format(datetime.datetime.now(), group, node.ip, data))
-
-        #control loop
-        while True:
-            gevent.sleep(10)
-            contiki_nodes = global_node_manager.get_mac_address_list()
-            print("\n")
-            print("Connected nodes", [str(node) for node in contiki_nodes])
-            if contiki_nodes:
-                #execute UPI function blocking
-                global_node_manager.execute_upi_function("radio","set_parameters",contiki_nodes,{'IEEE802154_phyCurrentChannel':12})
-
-                #schedule non-blocking UPI function with specific callback
-                exec_time = datetime.datetime.now() + datetime.timedelta(seconds=3)
-                global_node_manager.schedule_upi_function("radio","set_parameters",exec_time, contiki_nodes, print_response, {'IEEE802154_phyCurrentChannel':12})
-
-                #delayed UPI function call with default callback
-                global_node_manager.delay_upi_function("radio","set_parameters",3, contiki_nodes, {'IEEE802154_phyCurrentChannel':12})
 
     try:
         main(args)
