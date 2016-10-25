dd479a0e803b7dc0966ffd608ca2afd2f2b24862
Peter Ruckebusch <peter.ruckebusch@hotmail.com>
2016-10-25 22:27:31 +0200

    reformatting global cp, adding alternative version

--- contiki/sc2_tsch_blacklisting/global_cp2.py
+++ contiki/sc2_tsch_blacklisting/global_cp2.py
@@ -61,15 +61,18 @@ def mapWifiOnZigbeeChannels(log, channel_mapping):
 
 
 def ap_callback(group, node, cmd, data):
-    print("{} AP CALLBACK : Group: {}, NodeName: {}, Cmd: {}, Returns: {}".format(datetime.datetime.now(), group, node.name, cmd, data))
+    print("{} AP CALLBACK : Group: {}, NodeName: {}, Cmd: {}, Returns: {}".format(
+        datetime.datetime.now(), group, node.name, cmd, data))
 
 
 def sta_callback(group, node, cmd, data):
-    print("{} STA CALLBACK : Group: {}, NodeName: {}, Cmd: {}, Returns: {}".format(datetime.datetime.now(), group, node.name, cmd, data))
+    print("{} STA CALLBACK : Group: {}, NodeName: {}, Cmd: {}, Returns: {}".format(
+        datetime.datetime.now(), group, node.name, cmd, data))
 
 
 def default_callback(group, node, cmd, data):
-    print("{} DEFAULT CALLBACK : Group: {}, NodeName: {}, Cmd: {}, Returns: {}".format(datetime.datetime.now(), group, node.name, cmd, data))
+    print("{} DEFAULT CALLBACK : Group: {}, NodeName: {}, Cmd: {}, Returns: {}".format(
+        datetime.datetime.now(), group, node.name, cmd, data))
 
 
 def print_response(group, node, data):
@@ -82,34 +85,37 @@ def event_cb(mac_address, event_name, event_value):
 
 def main(args, interferer_ap, interferer_sta):
     contiki_nodes = global_node_manager.get_mac_address_list()
+    contiki_nodes.sort()
     print("Connected nodes", [str(node) for node in contiki_nodes])
     taisc_manager = TAISCMACManager(global_node_manager, "CSMA")
     app_manager = AppManager(global_node_manager)
 
     """ Blacklist TSCH channels that overlap with Wi-Fi channel """
-    wifi_to_tsch_channels_dct = mapWifiOnZigbeeChannels(log, './sc2_tsch_blacklisting/ieee80211_to_ieee802154_channels.csv')
+    wifi_to_tsch_channels_dct = mapWifiOnZigbeeChannels(
+        log, './sc2_tsch_blacklisting/ieee80211_to_ieee802154_channels.csv')
 
     ret = taisc_manager.update_slotframe('./contiki_helpers/default_taisc_slotframe.csv')
     log.info(ret)
     ret = taisc_manager.update_macconfiguration({'IEEE802154e_macSlotframeSize': len(contiki_nodes)})
     log.info(ret)
-    ret = taisc_manager.activate_radio_program("TSCH")
+    ret = taisc_manager.activate_radio_program("TSCH", contiki_nodes)
     log.info(ret)
     # global_node_manager.start_local_monitoring_cp()
     # gevent.sleep(5)
     # app_manager.subscribe_events(["RIME_appPerPacket_rxstats"], event_cb, 0)
     # gevent.sleep(5)
-    ret = app_manager.update_configuration({"RIME_exampleUnicastSendInterval": 37})
+    ret = app_manager.update_configuration({"RIME_exampleUnicastSendInterval": 37}, contiki_nodes)
     log.info(ret)
     ap_local_cp = global_node_manager.start_custom_local_cp(wifi_interference_ap, ap_callback, [interferer_ap])
     sta_local_cp = global_node_manager.start_custom_local_cp(wifi_interference_sta, sta_callback, [interferer_sta])
     prev_stats = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ret = app_manager.update_configuration({"RIME_exampleUnicastActivateApplication": 1}, contiki_nodes)
+    log.info("Activate APP {}".format(ret))
 
     # control loop
     while True:
         # without interference
         log.info("TSCH MAC without interference!")
-        err1 = app_manager.update_configuration({"RIME_exampleUnicastActivateApplication": 1})
         for i in range(0, 30):
             stats = taisc_manager.get_measurements(["IEEE802154_MACSTATS"], 1)[1]["IEEE802154_MACSTATS"]
             measurement = [int(time.time()), 1, 107, "TSCH", stats[0]]
@@ -118,15 +124,11 @@ def main(args, interferer_ap, interferer_sta):
             prev_stats = stats
             measurement_logger.log_measurement("IEEE802154_MACSTATS", measurement)
             gevent.sleep(1)
-        err2 = app_manager.update_configuration({"RIME_exampleUnicastActivateApplication": 0})
-        log.info("Activate APP {}, deactivate APP{}".format(err1, err2))
 
         # with interference
         log.info("TSCH MAC with interference!")
         ap_local_cp.send({'command': 'start_wifi_interference'})
-        gevent.sleep(5)
         sta_local_cp.send({'command': 'start_wifi_interference'})
-        err1 = app_manager.update_configuration({"RIME_exampleUnicastActivateApplication": 1})
         for i in range(0, 30):
             stats = taisc_manager.get_measurements(["IEEE802154_MACSTATS"], 1)[1]["IEEE802154_MACSTATS"]
             measurement = [int(time.time()), 1, 107, "TSCH", stats[0]]
@@ -135,16 +137,13 @@ def main(args, interferer_ap, interferer_sta):
             prev_stats = stats
             measurement_logger.log_measurement("IEEE802154_MACSTATS", measurement)
             gevent.sleep(1)
-        err2 = app_manager.update_configuration({"RIME_exampleUnicastActivateApplication": 0})
-        log.info("Activate APP {}, deactivate APP{}".format(err1, err2))
 
         # with interference + blacklisting
         # Blacklist channels
         log.info("TSCH MAC with interference, but blacklisted channels 15.4!")
         blacklisted_channels = wifi_to_tsch_channels_dct[6]
-        ret = taisc_manager.blacklist_channels(blacklisted_channels)
+        ret = taisc_manager.blacklist_channels(blacklisted_channels, contiki_nodes)
         log.info("Blacklist TSCH channels {}, error {}".format(blacklisted_channels, ret))
-        err1 = app_manager.update_configuration({"RIME_exampleUnicastActivateApplication": 1})
         for i in range(0, 30):
             stats = taisc_manager.get_measurements(["IEEE802154_MACSTATS"], 1)[1]["IEEE802154_MACSTATS"]
             measurement = [int(time.time()), 1, 107, "TSCH", stats[0]]
@@ -153,17 +152,13 @@ def main(args, interferer_ap, interferer_sta):
             prev_stats = stats
             measurement_logger.log_measurement("IEEE802154_MACSTATS", measurement)
             gevent.sleep(1)
-        err2 = app_manager.update_configuration({"RIME_exampleUnicastActivateApplication": 0})
-        log.info("Activate APP {}, deactivate APP{}".format(err1, err2))
 
         # Resetting state for next run
-        err1 = app_manager.update_configuration({"RIME_exampleUnicastActivateApplication": 0})
         ap_local_cp.send({'command': 'stop_wifi_interference'})
-        gevent.sleep(5)
         sta_local_cp.send({'command': 'stop_wifi_interference'})
-        err2 = taisc_manager.update_macconfiguration({'IEEE802154e_macHoppingSequenceList': [16, 18, 17, 23, 26, 15, 25, 22, 19, 11, 12, 13, 24, 14, 20, 21]})
-        err3 = taisc_manager.update_macconfiguration({'IEEE802154e_macHoppingSequenceLength': 16})
-        log.info('resetting state for next run {} {} {}'.format(err1, err2, err3))
+        err1 = taisc_manager.update_macconfiguration({'IEEE802154e_macHoppingSequenceList': [16, 18, 17, 23, 26, 15, 25, 22, 19, 11, 12, 13, 24, 14, 20, 21]}, contiki_nodes)
+        err2 = taisc_manager.update_macconfiguration({'IEEE802154e_macHoppingSequenceLength': 16}, contiki_nodes)
+        log.info('resetting state for next run {} {}'.format(err1, err2))
 
 
 if __name__ == "__main__":
@@ -190,7 +185,8 @@ if __name__ == "__main__":
     if args['--logfile']:
         logfile = args['--logfile']
 
-    logging.basicConfig(filename=logfile, level=log_level, format='%(asctime)s - %(name)s.%(funcName)s() - %(levelname)s - %(message)s')
+    logging.basicConfig(filename=logfile, level=log_level,
+                        format='%(asctime)s - %(name)s.%(funcName)s() - %(levelname)s - %(message)s')
 
     log.debug(args)
 
