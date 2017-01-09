98728ce8a64779efac0bcc15607cce4cc02b3a6a
Maicon Kist <maiconkist@gmail.com>
2017-01-09 19:59:12 +0000

    update

--- gnuradio/virtual_radio/wishful_simple_controller.py
+++ gnuradio/virtual_radio/wishful_simple_controller.py
@@ -9,6 +9,7 @@ import wishful_upis as upis
 import os
 import sys
 import time
+import pickle
 import wishful_module_gnuradio
 
 import threading
@@ -39,6 +40,41 @@ nodes = {}
 the_node = None
 the_variables = {}
 
+TOTAL_NODES = 2
+NODE_NAMES = ["tx", "rx1", "rx2"]
+
+# list of files that will be send to agents
+files = {
+    "tx" : 	"/home/nodeuser/gr-hydra/apps/video_benchmark_tx.py",
+    "rx" :  "/home/nodeuser/gr-hydra/apps/video_rx.py"
+}
+
+# What code each node will execute. Node 0 is TX, Node 1 is RX
+nodes_to_program = {
+    "tx" : "tx",
+    "rx1": "rx", 
+    "rx2": "rx", 
+}
+
+program_getters = {
+    "tx": ["svl_bandwidth", "svl_center_freq"],
+    "rx": ["bandwidth", "center_freq", "pkt_rcvd", "pkt_right", ],
+}
+
+
+# for the svl tx
+SVL_TX_BANDWIDTH = 'svl_bandwidth'
+SVL_TX_CENTER_FREQ = 'svl_center_freq'
+VR_TX_GAIN = 'vr{id}_gain'
+
+# for the receivers only
+RX_BANDWIDTH = 'bandwidth'
+RX_CENTER_FREQ = 'center_freq'
+RX_GAIN = 'gain'
+
+SETTER_FILE = "./setter.bin"
+
+
 @controller.new_node_callback()
 def new_node(node):
     log.info("New node appeared:")
@@ -61,49 +97,21 @@ def default_callback(group, node, cmd, data):
 def get_vars_response(group, node, data):
     log.info("{} get_channel_reponse : Group:{}, NodeId:{}, msg:{}".format(datetime.datetime.now(), group, node.id, data))
 
-    if sys.argv[1] == '--write-to-file':
-            if node.name == 'rx1':
-                the_variables['rx1_pkt_rcv'] = data['pkt_rcvd'] if 'pkt_rcvd' in data else 'NA'
-                the_variables['rx1_pkt_right'] = data['pkt_right'] if 'pkt_right' in data else 'NA'
-                the_variables['rx1_center_freq'] = data['center_freq'] if 'center_freq' in data else 'NA'
-                the_variables['rx1_bandwidth'] = data['bandwidth'] if 'bandwidth' in data else 'NA'
-            elif node.name == 'rx2':
-                the_variables['rx2_pkt_rcv'] = data['pkt_rcvd'] if 'pkt_rcvd' in data else 'NA'
-                the_variables['rx2_pkt_right'] = data['pkt_right'] if 'pkt_right' in data else 'NA'
-                the_variables['rx2_center_freq'] = data['center_freq'] if 'center_freq' in data else 'NA'
-                the_variables['rx2_bandwidth'] = data['bandwidth'] if 'bandwidth' in data else 'NA'
+    if node.name == 'rx1':
+        the_variables['rx1_pkt_rcv'] = data['pkt_rcvd'] if 'pkt_rcvd' in data else 'NA'
+        the_variables['rx1_pkt_right'] = data['pkt_right'] if 'pkt_right' in data else 'NA'
+        the_variables['rx1_center_freq'] = data['center_freq'] if 'center_freq' in data else 'NA'
+        the_variables['rx1_bandwidth'] = data['bandwidth'] if 'bandwidth' in data else 'NA'
+    #elif node.name == 'rx2':
+        the_variables['rx2_pkt_rcv'] = data['pkt_rcvd'] if 'pkt_rcvd' in data else 'NA'
+        the_variables['rx2_pkt_right'] = data['pkt_right'] if 'pkt_right' in data else 'NA'
+        the_variables['rx2_center_freq'] = data['center_freq'] if 'center_freq' in data else 'NA'
+        the_variables['rx2_bandwidth'] = data['bandwidth'] if 'bandwidth' in data else 'NA'
 
-            import pickle
-            pickle.dump(the_variables, open('controller_data.bin', 'wb'))
- 
+
+    pickle.dump(the_variables, open('controller_data.bin', 'wb'))
 
 def exec_loop():
-    TOTAL_NODES = 2
-    NODE_NAMES = ["tx", "rx1", "rx2"]
-
-    # list of files that will be send to agents
-    files = {
-        "tx" : 	"/home/nodeuser/gr-hydra/apps/video_benchmark_tx.py",
-	    "rx" :  "/home/nodeuser/gr-hydra/apps/video_rx.py"
-    }
-
-    # What code each node will execute. Node 0 is TX, Node 1 is RX
-    nodes_to_program = {
-	    "tx" : "tx",
-        "rx1": "rx", 
-        "rx2": "rx", 
-    }
-
-    program_getters = {
-        "tx": ["svl_bandwidth", "svl_center_freq"],
-        "rx": ["bandwidth", "center_freq", "pkt_rcvd", "pkt_right", ],
-    }
-
-    program_setters = {
-        "tx": ["svl_bandwidth", "svl_center_freq"],
-        "rx": ["andwidth", "center_freq", "gain"],
-    }
-
     # open files, read content, keep it in codes dict
     program = {}
     for _k, _v in files.items():
@@ -136,8 +144,32 @@ def exec_loop():
     #control loop
     while nodes:
             # TRICKY: gets are assynchronous. callback for get_parameters is called automatically
-            log.info("Requesting values to VR 1 RX")
-            controller.blocking(False).node(nodes['rx1']).radio.iface('usrp').get_parameters(program_getters['rx'])
+
+            if 'rx1' in nodes:
+                log.info("Requesting values to VR 1 RX")
+                controller.blocking(False).node(nodes['rx1']).radio.iface('usrp').get_parameters(program_getters['rx'])
+
+            if 'rx2' in nodes:
+                log.info("Requesting values to VR 2 RX")
+                controller.blocking(False).node(nodes['rx2']).radio.iface('usrp').get_parameters(program_getters['rx'])
+
+
+            try:
+                setters = pickle.load(open(SETTER_FILE, "rb"))
+
+                if 'tx' in setters:
+                    log.info("Setting values to SVL")
+                    controller.blocking(False).node(nodes['tx']).radio.iface('usrp').set_parameters(setters['tx'])
+                if 'rx1' in setters:
+                    log.info("Setting values to VR 1 RX")
+                    controller.blocking(False).node(nodes['rx1']).radio.iface('usrp').set_parameters(setters['rx1'])
+                if 'rx2' in setters:
+                    log.info("Setting values to VR 1 RX")
+                    controller.blocking(False).node(nodes['rx2']).radio.iface('usrp').set_parameters(setters['rx2'])
+
+            except Exception as e:
+                log.info("setters file for vr1 not found. Nothing to change" )
+                log.info(e)
 
             """
             if running:
@@ -149,8 +181,6 @@ def exec_loop():
                 #controller.blocking(False).node(nodes[0]).radio.iface('usrp').activate_radio_program({'program_name': program_name})
                 running = True
             """
-
-
             gevent.sleep(2)
 
     log.info("All nodes disconnected. Exiting controller")
--- gnuradio/virtual_radio/wishful_simple_controller_gui.py
+++ gnuradio/virtual_radio/wishful_simple_controller_gui.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 from tkinter import *
-from wishful_simple_controller import *
-
 import pickle
 import subprocess
+
+import wishful_simple_controller as wsc
 
 controller_pid = None
 widget = None
@@ -14,13 +14,11 @@ from tkinter import *
 class AppGui():
 
     def __init__(self, root):
-
-        self.vr1_pkt_recv_info, self.vr1_pkt_right_info, self.vr1_tx_amplitude = self.build_vr_menu(1, root)
-
-        print(self.vr1_pkt_right_info)
+        self.vr1_pkt_recv_info, self.vr1_pkt_right_info = self.build_vr_menu(1, root, self.vr1_tx_amplitude_update)
+        self.vr2_pkt_recv_info, self.vr2_pkt_right_info = self.build_vr_menu(2, root, self.vr2_tx_amplitude_update)
 
 
-    def build_vr_menu(self, vr_id, root):
+    def build_vr_menu(self, vr_id, root, tx_amplitude_callback):
         vr_frame = LabelFrame(root, text="Virtual Radio %d" % (vr_id, ))
         vr_frame.pack(fill="both", expand="yes")
 
@@ -39,29 +37,46 @@ class AppGui():
         vr_pkt_right_frame.pack()
 
         # vr tx-amplitude
-        vr_tx_amplitude = DoubleVar()
         vr_tx_amplitude_frame = Frame(vr_frame)
         Label(vr_tx_amplitude_frame, text="TX Amplitude [0:1]: ").pack(side=LEFT)
-        vr_tx_amplitude_scale = Scale(vr_tx_amplitude_frame, variable = vr_tx_amplitude, from_ = 0.0, to = 1.0, resolution= 0.001, orient = HORIZONTAL).pack(anchor=CENTER)
+        vr_tx_amplitude_scale = Scale(vr_tx_amplitude_frame, from_ = 0.0, to = 1.0, resolution= 0.001, orient = HORIZONTAL, command=tx_amplitude_callback).pack(anchor=CENTER)
         vr_tx_amplitude_frame.pack()
 
-        return vr_pkt_recv_info, vr_pkt_right_info, vr_tx_amplitude
+        return vr_pkt_recv_info, vr_pkt_right_info
+
+    def vr1_tx_amplitude_update(self, val):
+        sets = {}
+
+        try:
+            sets = pickle.load(open(wsc.SETTER_FILE, "rb"))
+        except:
+            pass
+
+        if not 'tx' in sets:
+            sets['tx'] = {}
+
+        sets['tx'][wsc.VR_TX_GAIN.format(id=1)] = val
+        pickle.dump(sets, open(wsc.SETTER_FILE, "wb"))
+
+    def vr2_tx_amplitude_update(self, val):
+        print(val)
 
 
 def update():
-
     try:
-        the_variables = pickle.load(open("controller_data.bin", "rb" ) )
+        the_variables = pickle.load(open("controller_data.bin", "rb"))
 
         widget.vr1_pkt_recv_info['text']  = the_variables['rx1_pkt_rcv']
         widget.vr1_pkt_right_info['text'] = the_variables['rx1_pkt_right']
+
+        widget.vr2_pkt_recv_info['text']  = the_variables['rx2_pkt_rcv']
+        widget.vr2_pkt_right_info['text'] = the_variables['rx2_pkt_right']
 
     except Exception as e:
         print(e)
 
     root.after(1000, update)
     
-
 if __name__ == '__main__':
     root = Tk()
     widget = AppGui(root)
@@ -70,7 +85,6 @@ if __name__ == '__main__':
 
     root.after(1000, update)
     root.mainloop()
-
 
     controller_pid.communicate('exit\n')
     controller_pid.kill()
